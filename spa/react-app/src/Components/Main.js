b1fda83f6fd30f23e23d991b4970ad4ca9bdec9e
LCinder <diegohm1598@gmail.com>
2022-05-05 18:15:50 +0200

    Dia 05/005 terminado

--- spa/react-app/src/Components/Main.js
+++ spa/react-app/src/Components/Main.js
@@ -1,14 +1,21 @@
 
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import Table from "./Table"
+    import * as React from 'react';
+    import CssBaseline from '@mui/material/CssBaseline';
+    import Box from '@mui/material/Box';
+    import Typography from '@mui/material/Typography';
+    import Container from '@mui/material/Container';
+    import Link from '@mui/material/Link';
+    import Table from "./Table"
+    import {useState} from 'react'
 
-function Copyright() {
-  return (
+
+    function createData(name, status) {
+    return { name, status };
+    }
+
+
+    function Copyright() {
+    return (
     <Typography variant="body2" color="text.secondary">
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
@@ -17,54 +24,60 @@ function Copyright() {
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
-}
+    );
+    }
 
 export default function Main() {
+    const [data, setData] = useState([
+      createData("Task 1", "Complete"),
+      createData("Task 2", "Incomplete"),
+      createData("Task 3", "Complete"),
+      createData("Task 4", "Complete"),
+    ]);
 
-    const data = [
-        {"data1": "data1"}
-    ]
 
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-      }}
-    >
-      <CssBaseline />
-      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          SPA para SSBW
-        </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {'Pin a footer to the bottom of the viewport.'}
-          {'The footer will move as the main element of the page grows.'}
-        </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
-          <Table data={data}/>
-      </Container>
-      <Box
-        component="footer"
-        sx={{
-          py: 3,
-          px: 2,
-          mt: 'auto',
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[200]
-              : theme.palette.grey[800],
-        }}
-      >
-        <Container maxWidth="sm">
-          <Typography variant="body1">
-            My sticky footer can be found here.
-          </Typography>
-          <Copyright />
-        </Container>
-      </Box>
-    </Box>
-  );
+    const change = (index, event) => {
+        console.log(`${index} - ${event.target.checked}`)
+        const newData = [...data]
+        const statusIncomplete = "Incomplete"
+        const statusComplete = "Incomplete"
+
+        if (!event.target.checked)
+            newData[index].status = statusIncomplete
+        setData(newData)
+    }
+
+    return (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+          }}
+        >
+          <CssBaseline />
+          <Container component="main">
+            <Typography variant="h3" gutterBottom>
+              SPA para SSBW
+            </Typography>
+            <Table data={data} onChange={change}/>
+          </Container>
+          <Box
+            component="footer"
+            sx={{
+              py: 3,
+              px: 2,
+              mt: 'auto',
+              backgroundColor: (theme) =>
+                theme.palette.mode === 'light'
+                  ? theme.palette.grey[200]
+                  : theme.palette.grey[800],
+            }}
+          >
+            <Container maxWidth="sm">
+              <Copyright />
+            </Container>
+          </Box>
+        </Box>
+    );
 }
